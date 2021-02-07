32e7a8d701a654131795857574307c0f3bfe9a2c
Leonardo <leonardo_esqueda@uip.edu.pa>
2021-02-06 20:41:45 -0800

    Se creó una calculadora utilizando switch-case

--- src/com/leonardo/Main.java
+++ src/com/leonardo/Main.java
@@ -1,8 +1,87 @@
+/**
+ * Operadores Aritméticos
+ * = <- Asignación
+ * - Resta
+ * + Suma / concatenación
+ * *
+ * /
+ * %
+ *
+ * Operadores aritméticos incrementales
+ * ++ <- Sumar 1
+ * -- <- restar 1
+ *
+ * Operadores aritméticos combinados
+ * += <- a = a + b
+ * -=
+ * *=
+ * /=
+ * %=
+ *
+ * Operadores relacionales
+ * == <- igualdad
+ * != <- distinto a != b
+ * <
+ * >
+ * <=
+ * >=
+ *
+ * Operadores Lógicos
+ * ! <- Negación
+ * | <- Suma Lógica
+ * ^ <- xor
+ * &
+ * || <- true & true
+ *
+ * */
+
 package com.leonardo;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        int dato1, dato2, res=0;
+        int op;
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("1. Suma");
+        System.out.println("2. Resta");
+        System.out.println("3. Mult");
+        System.out.println("4. Div");
+
+        System.out.println("Ingrese el número de la operación");
+        op = scan.nextInt();
+
+        System.out.println("Ingrese el dato 1");
+        dato1 = scan.nextInt();
+        System.out.println("Ingrese el dato 2");
+        dato2 = scan.nextInt();
+
+        switch (op){
+            case 1:
+                res = dato1 + dato2;
+                break;
+
+            case 2:
+                res = dato1 - dato2;
+                break;
+
+            case 3:
+                res = dato1 * dato2;
+                break;
+            case 4:
+                res = dato1 / dato2;
+                break;
+
+            default:
+                System.out.println("Ingrese un valor válido");
+                break;
+        }
+
+        System.out.println("El resultado es: " + res);
+
     }
 }
